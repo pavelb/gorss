c137a4d5b4da7063c8007ff0efe9bf8042822858
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-05-25 19:54:33 -0700

    add reddit controller and embed package

---
+++ app/controllers/reddit.go
@@ -0,0 +1,119 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/robfig/revel"
+	"io/ioutil"
+	"net/http"
+	"rss/app/embed"
+	"rss/app/rss"
+	"sync"
+	"time"
+)
+
+type JSONFeed struct {
+	Data struct {
+		Children []struct {
+			Data JSONItem
+		}
+	}
+}
+
+type JSONItem struct {
+	Url         string
+	Title       string
+	Description string
+	Created_utc float64
+	Permalink   string
+}
+
+func getURL(url string) (bytes []byte, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func newRSSItem(jsonItem *JSONItem) *rss.Item {
+	comments := "http://reddit.com" + jsonItem.Permalink
+
+	args := &map[string]string{
+		"EmbedlyAPIKey": "8b02b918d50e4e33b9152d62985d6241",
+		"maxWidth":      "768",
+	}
+	description := fmt.Sprintf(
+		"%s<br/><a href='%s'>Comments</a>",
+		embed.GetMarkup(jsonItem.Url, args),
+		comments,
+	)
+
+	return &rss.Item{
+		Title:       jsonItem.Title,
+		Link:        jsonItem.Url,
+		Description: description,
+		Comments:    comments,
+		GUID:        jsonItem.Url,
+		PubDate:     time.Unix(int64(jsonItem.Created_utc), 0).Format(time.RFC822),
+	}
+}
+
+func newJSONFeed(subreddit string, limit int) (jsonFeed *JSONFeed, err error) {
+	bytes, err := getURL(fmt.Sprintf("http://www.reddit.com/r/%s.json?limit=%d", subreddit, limit))
+	if err != nil {
+		return
+	}
+	jsonFeed = new(JSONFeed)
+	err = json.Unmarshal(bytes, jsonFeed)
+	return
+}
+
+func newRSSFeed(subreddit string, limit int) (feed *rss.Feed, err error) {
+	jsonFeed, err := newJSONFeed(subreddit, limit)
+	if err != nil {
+		return
+	}
+
+	feed = &rss.Feed{
+		Version: 2.0,
+		Channel: rss.Channel{
+			Title:       "r/" + subreddit,
+			Link:        "http://www.reddit.com/r/" + subreddit,
+			Description: "Embelished version of 'r/" + subreddit + "' subreddit RSS feed",
+			TTL:         10,
+			Items:       make([]*rss.Item, len(jsonFeed.Data.Children)),
+		},
+	}
+
+	var wg sync.WaitGroup
+	for i := range jsonFeed.Data.Children {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			feed.Channel.Items[i] = newRSSItem(&jsonFeed.Data.Children[i].Data)
+		}(i)
+	}
+	wg.Wait()
+	return
+}
+
+type Reddit struct {
+	*revel.Controller
+}
+
+type Html string
+
+func (r Html) Apply(req *revel.Request, resp *revel.Response) {
+	resp.WriteHeader(http.StatusOK, "text/html")
+	resp.Out.Write([]byte(r))
+}
+
+func (c Reddit) Feed(r string) revel.Result {
+	feed, err := newRSSFeed(r, 100)
+	if err != nil {
+		return Html(fmt.Sprint(err))
+	}
+	return c.RenderXml(feed)
+}
---
+++ app/embed/embed.go
@@ -0,0 +1,139 @@
+package embed
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+// Check out http://noembed.com/
+
+type Strategy func(string, *map[string]string) (string, error)
+
+func getURL(url string) (bytes []byte, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func imageMarkup(url string) string {
+	return fmt.Sprintf("<img style='max-width:100%%' src='%s'/>", url)
+}
+
+func embedImage(url string, args *map[string]string) (markup string, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	if header, ok := resp.Header["Content-Type"]; ok {
+		for _, mimeType := range header {
+			const prefix = "image"
+			if len(mimeType) >= len(prefix) && mimeType[:5] == prefix {
+				return imageMarkup(url), nil
+			}
+		}
+	}
+	return
+}
+
+func embedExtensionlessImage(url string, args *map[string]string) (markup string, err error) {
+	return embedImage(strings.TrimRight(url, "/")+".png", args)
+}
+
+func embedImgurGalleryImage(url string, args *map[string]string) (markup string, err error) {
+	return embedExtensionlessImage(strings.Replace(url, "/gallery", "", 1), args)
+}
+
+func oembed(mustMatch string, endpoint string, uri string, args *map[string]string) (markup string, err error) {
+	var matcher = regexp.MustCompile(mustMatch)
+	if !matcher.MatchString(uri) {
+		return
+	}
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return
+	}
+	q := u.Query()
+	q.Set("format", "json")
+	q.Set("url", uri)
+	if maxWidth, ok := (*args)["maxWidth"]; ok {
+		q.Set("maxwidth", maxWidth)
+	}
+	u.RawQuery = q.Encode()
+	bytes, err := getURL(u.String())
+	if err != nil {
+		return
+	}
+	response := make(map[string]interface{})
+	err = json.Unmarshal(bytes, &response)
+	if err != nil {
+		return
+	}
+	if html, ok := response["html"]; ok {
+		return html.(string), nil
+	}
+	if resType, ok := response["type"]; ok {
+		switch resType {
+		case "photo":
+			if resUrl, ok := response["url"]; ok {
+				return imageMarkup(resUrl.(string)), nil
+			}
+		case "link":
+			if description, ok := response["description"]; ok {
+				return description.(string), nil
+			}
+		}
+	}
+	return
+}
+
+func addOembedStrategies(strategies *[]Strategy) {
+	providers := map[string]string{
+		"http://www.youtube.com/oembed":           "youtu",
+		"http://www.flickr.com/services/oembed":   "flickr",
+		"http://lab.viddler.com/services/oembed":  "viddler",
+		"http://qik.com/api/oembed.json":          "qik",
+		"http://revision3.com/api/oembed":         "revision3",
+		"http://www.hulu.com/api/oembed.json":     "hulu",
+		"http://vimeo.com/api/oembed.json":        "vimeo",
+		"http://www.collegehumor.com/oembed.json": "collegehumor",
+		"http://api.imgur.com/oembed":             "imgur",
+	}
+	// if apiKey, ok := (*args)["EmbedlyAPIKey"]; ok {
+	// 	providers["http://api.embed.ly/1/oembed?key="+apiKey] = ""
+	// }
+	for endpoint, mustMatch := range providers {
+		*strategies = append(*strategies, func(mustMatch string, endpoint string) Strategy {
+			return func(url string, args *map[string]string) (markup string, err error) {
+				return oembed(mustMatch, endpoint, url, args)
+			}
+		}(mustMatch, endpoint))
+	}
+}
+
+func GetMarkup(url string, args *map[string]string) string {
+	strategies := []Strategy{
+		embedImage,
+		embedExtensionlessImage,
+		embedImgurGalleryImage,
+	}
+	addOembedStrategies(&strategies)
+
+	for _, strategy := range strategies {
+		markup, err := strategy(url, args)
+		if err != nil {
+			fmt.Sprintln("Error during getMarkup('%s'): %s", url, err)
+		} else if markup != "" {
+			return markup
+		}
+	}
+	return "..."
+}
